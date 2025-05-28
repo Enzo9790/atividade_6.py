10be32c74e7737a044c2c35fc949a796bc10c7d1
Enzo9790 <99890071+Enzo9790@users.noreply.github.com>
2025-05-28 15:37:53 -0300

    Add files via upload

---
+++ aula6.py
@@ -0,0 +1,53 @@
+# indentificação em python - organizar o código py
+
+variavel = 'teste'
+
+    
+if variavel == 'teste':
+    print('ok')
+    
+for n in range(1,10):
+    print(n)
+    
+else:
+    print('teste 2')
+    
+# tipo de organização do código    
+if 1 > 10:
+  x = 10
+  
+# variavel precisa estar escostada ma borda
+# quando não estiver dentro de nenhum escopo
+
+x = 10
+
+# quando estiver dentro do escopo
+# precisar te pelo menos 1 espaço
+# O padrão são 4 espaços
+
+if x == 10:
+    print(x)
+    
+# Concatenação
+
+nome = 'Maria'
+print('Olá, seja bem vindo', nome)
+print('Olá, seja bem vindo'+' '+ nome)
+print('Olá, seja bem vindo {}'.format (nome))
+print('Olá, seja bem vindo %s'%(nome))
+print(f'Olá, seja bem vindo {nome}')
+
+# Refatorar + melhorar o código
+# encontar erros
+nome = 'Julio'
+sobrenome = 'Cesar'
+
+print('Hello', nome, sobrenome)
+
+#encontrar erros como espaçamento
+
+c = 10
+
+
+
+    
---
+++ exercícios_aula6.py
@@ -0,0 +1,32 @@
+# Multiplique 4 por 6 e divida o resultado por 2.
+resultado =(4 * 6) / 2
+print('resultado', resultado)
+
+#Caucule a soma de 5 e 7
+resultado =( 5 + 7)
+print('resultado', resultado)
+
+#Eleve 3 à potência de 4
+resultado =(3 ** 4)
+print('resultado',resultado)
+
+#Caucule a média de 8,12 e 15
+n1 = 8
+n2 = 12
+n3 = 15
+calculo =(n1 + n2 + n3) /3
+print('resultado', calculo)
+
+# Subtraia 10 de 2 e mutiplique o resultado por 5.
+n1 = 10
+n2 = 2
+n3 = 5
+calculo = (n1 - n2)*n3
+print(' resultado',calculo)
+
+#Divida 27 por 3 e some 5
+n1 = 27
+n2 = 3
+n3 = 5
+calculo = ( n1 / n2)+5
+print(' resultado',calculo)
